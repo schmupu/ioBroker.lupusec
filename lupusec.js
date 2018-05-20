8cf43fa5493b926c35a5a4162235185bb3de4c9d
Thorsten Stueben <thorsten.stueben@macbook.local>
2018-05-20 23:04:09 +0200

    Lupusec Rest Services lesen!

--- lupusec.js
+++ lupusec.js
@@ -1,110 +1,124 @@
-
 'use strict';
 
 // you have to require the utils module and call adapter function
-var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
-var lupus =    require(__dirname + '/lib/lupus');
+var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
+var Lupus = require(__dirname + '/lib/lupus');
 var adapter = new utils.Adapter('lupusec');
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
-adapter.on('unload', function (callback) {
-    try {
-        adapter.log.info('cleaned everything up...');
-        callback();
-    } catch (e) {
-        callback();
-    }
+adapter.on('unload', function(callback) {
+  try {
+    adapter.log.info('cleaned everything up...');
+    callback();
+  } catch (e) {
+    callback();
+  }
 });
 
 // is called if a subscribed object changes
-adapter.on('objectChange', function (id, obj) {
-    // Warning, obj can be null if it was deleted
-    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+adapter.on('objectChange', function(id, obj) {
+  // Warning, obj can be null if it was deleted
+  adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
 });
 
 // is called if a subscribed state changes
-adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
-    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+adapter.on('stateChange', function(id, state) {
+  // Warning, state can be null if it was deleted
+  adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
 
-    // you can use the ack flag to detect if it is status (true) or command (false)
-    if (state && !state.ack) {
-        adapter.log.info('ack is not set!');
-    }
+  // you can use the ack flag to detect if it is status (true) or command (false)
+  if (state && !state.ack) {
+    adapter.log.info('ack is not set!');
+  }
 });
 
 // Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
-adapter.on('message', function (obj) {
-    if (typeof obj === 'object' && obj.message) {
-        if (obj.command === 'send') {
-            // e.g. send email or pushover or whatever
-            console.log('send command');
+adapter.on('message', function(obj) {
+  if (typeof obj === 'object' && obj.message) {
+    if (obj.command === 'send') {
+      // e.g. send email or pushover or whatever
+      console.log('send command');
 
-            // Send response in callback if required
-            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-        }
+      // Send response in callback if required
+      if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
     }
+  }
 });
 
 // is called when databases are connected and adapter received configuration.
 // start here!
-adapter.on('ready', function () {
-    main();
+adapter.on('ready', function() {
+  main();
 });
 
 function main() {
 
-    // The adapters config (in the instance object everything under the attribute "native") is accessible via
-    // adapter.config:
-    adapter.log.info('config test1: '    + adapter.config.test1);
-    adapter.log.info('config test1: '    + adapter.config.test2);
-    adapter.log.info('config mySelect: ' + adapter.config.mySelect);
+  // The adapters config (in the instance object everything under the attribute "native") is accessible via
+  // adapter.config:
+  adapter.log.info('config test1: ' + adapter.config.test1);
+  adapter.log.info('config test1: ' + adapter.config.test2);
+  adapter.log.info('config mySelect: ' + adapter.config.mySelect);
 
 
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple lupusec for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
+  /**
+   *
+   *      For every state in the system there has to be also an object of type state
+   *
+   *      Here a simple lupusec for a boolean variable named "testVariable"
+   *
+   *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+   *
+   */
 
-    adapter.setObject('testVariable', {
-        type: 'state',
-        common: {
-            name: 'testVariable',
-            type: 'boolean',
-            role: 'indicator'
-        },
-        native: {}
-    });
+  adapter.setObject('testVariable', {
+    type: 'state',
+    common: {
+      name: 'testVariable',
+      type: 'boolean',
+      role: 'indicator'
+    },
+    native: {}
+  });
 
-    // in this lupusec all states changes inside the adapters namespace are subscribed
-    adapter.subscribeStates('*');
+  // in this lupusec all states changes inside the adapters namespace are subscribed
+  adapter.subscribeStates('*');
 
 
-    /**
-     *   setState examples
-     *
-     *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-     *
-     */
+  /**
+   *   setState examples
+   *
+   *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+   *
+   */
 
-    // the variable testVariable is set to true as command (ack=false)
-    adapter.setState('testVariable', true);
+  // the variable testVariable is set to true as command (ack=false)
+  adapter.setState('testVariable', true);
 
-    // same thing, but the value is flagged "ack"
-    // ack should be always set to true if the value is received from or acknowledged from the target system
-    adapter.setState('testVariable', {val: true, ack: true});
+  // same thing, but the value is flagged "ack"
+  // ack should be always set to true if the value is received from or acknowledged from the target system
+  adapter.setState('testVariable', {
+    val: true,
+    ack: true
+  });
 
-    // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
+  // same thing, but the state is deleted after 30s (getState will return null afterwards)
+  adapter.setState('testVariable', {
+    val: true,
+    ack: true,
+    expire: 30
+  });
 
-    // lupus.test(adapter);
+  // lupus.test(adapter);
 
-    var t = new lupus(adapter);
-    t.show();
+
+  var t = new Lupus(
+    adapter,
+    adapter.config.alarm_host,
+    adapter.config.alarm_https,
+    adapter.config.alarm_user,
+    adapter.config.alarm_password,
+  );
+
+  t.test();
 
 }
