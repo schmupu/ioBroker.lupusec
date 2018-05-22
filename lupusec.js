8c962f71cfea29f02122477cc8ab72bf02b389d1
Thorsten Stueben <thorsten.stueben@macbook.local>
2018-05-22 20:17:39 +0200

    .

--- lupusec.js
+++ lupusec.js
@@ -8,7 +8,7 @@ var adapter = new utils.Adapter('lupusec');
 // is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function(callback) {
   try {
-    adapter.log.info('cleaned everything up...');
+    // adapter.log.info('cleaned everything up...');
     callback();
   } catch (e) {
     callback();
@@ -18,7 +18,7 @@ adapter.on('unload', function(callback) {
 // is called if a subscribed object changes
 adapter.on('objectChange', function(id, obj) {
   // Warning, obj can be null if it was deleted
-  adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+  // adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
 });
 
 // is called if a subscribed state changes
@@ -28,8 +28,9 @@ adapter.on('stateChange', function(id, state) {
 
   // you can use the ack flag to detect if it is status (true) or command (false)
   if (state && !state.ack) {
-    adapter.log.info('ack is not set!');
+  //  adapter.log.info('ack is not set!');
   }
+
 });
 
 // Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
@@ -37,7 +38,7 @@ adapter.on('message', function(obj) {
   if (typeof obj === 'object' && obj.message) {
     if (obj.command === 'send') {
       // e.g. send email or pushover or whatever
-      console.log('send command');
+      // console.log('send command');
 
       // Send response in callback if required
       if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
@@ -53,22 +54,6 @@ adapter.on('ready', function() {
 
 function main() {
 
-  // The adapters config (in the instance object everything under the attribute "native") is accessible via
-  // adapter.config:
-  adapter.log.info('config test1: ' + adapter.config.test1);
-  adapter.log.info('config test1: ' + adapter.config.test2);
-  adapter.log.info('config mySelect: ' + adapter.config.mySelect);
-
-
-  /**
-   *
-   *      For every state in the system there has to be also an object of type state
-   *
-   *      Here a simple lupusec for a boolean variable named "testVariable"
-   *
-   *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-   *
-   */
 
   adapter.setObject('testVariable', {
     type: 'state',
@@ -80,39 +65,11 @@ function main() {
     native: {}
   });
 
-  // in this lupusec all states changes inside the adapters namespace are subscribed
-  adapter.subscribeStates('*');
-
-
-  /**
-   *   setState examples
-   *
-   *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-   *
-   */
-
-  // the variable testVariable is set to true as command (ack=false)
-  adapter.setState('testVariable', true);
-
-  // same thing, but the value is flagged "ack"
-  // ack should be always set to true if the value is received from or acknowledged from the target system
-  adapter.setState('testVariable', {
-    val: true,
-    ack: true
-  });
-
-  // same thing, but the state is deleted after 30s (getState will return null afterwards)
-  adapter.setState('testVariable', {
-    val: true,
-    ack: true,
-    expire: 30
-  });
-
-  // lupus.test(adapter);
-
 
   var t = new Lupus(adapter);
 
   t.test();
+
+  adapter.subscribeStates('*');
 
 }
