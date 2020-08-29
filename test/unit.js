8056c179389d078bfee20099f4b8bc1e7bd69201
Thorsten Stueben <thorsten.stueben@macbook.local>
2020-08-29 12:50:21 +0200

    Test

--- test/unit.js
+++ test/unit.js
@@ -5,3 +5,10 @@ const { tests } = require('@iobroker/testing');
 tests.unit(path.join(__dirname, '..'), {
     allowedExitCodes: [11],
 });
+
+// Run tests
+tests.integration(path.join(__dirname, ".."), {
+    // If the adapter may call process.exit during startup, define here which exit codes are allowed.
+    // By default, termination during startup is not allowed.
+    allowedExitCodes: [11],
+}); 
